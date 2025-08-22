e2275b2a64a769177a778dc3cf987897db2d6f4f
caio poleza <caio_p_santos@estudante.sesisenai.org.br>
2025-08-22 09:04:08 -0300

    add-deletematch

---
+++ private/Match/deleteMatch.php
@@ -0,0 +1,20 @@
+<?php
+<?php
+include '../../config/db.php';
+
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+
+    $stmt = $conn->prepare("DELETE FROM partidas WHERE id = ?");
+    $stmt->bind_param("i", $id);
+
+    if ($stmt->execute()) {
+        header("Location: listMatch.php?msg=Partida excluída com sucesso!");
+        exit;
+    } else {
+        echo "<p style='color:red;'>Erro ao excluir partida.</p>";
+    }
+} else {
+    echo "<p style='color:red;'>ID da partida não informado.</p>";
+}
+?>
