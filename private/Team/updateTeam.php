a3897f0b10601e56b69d477389c400ee138df0b6
kaua <kaua_r_ribeiro@estudante.sesisenai.org.br>
2025-08-22 09:40:46 -0300

    ADD - sei la

---
+++ private/Team/updateTeam.php
@@ -0,0 +1,59 @@
+<?php
+
+include '../../config/db.php';
+
+$id = $_GET['id'];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $nome = $_POST['nome'];
+    $cidade = $_POST['cidade'];
+
+    $sql = "UPDATE times SET nome ='$nome',cidade ='$cidade' WHERE id=$id";
+
+    if ($conn->query($sql) === true) {
+        echo "Time atualizado com sucesso.
+        <a href='readTeam.php'>Ver times.</a>
+        ";
+    } else {
+        echo "Erro " . $sql . '<br>' . $conn->error;
+    }
+    $conn->close();
+    exit(); 
+}
+
+$sql = "SELECT * FROM times WHERE id=$id";
+$result = $conn -> query($sql);
+$row = $result -> fetch_assoc();
+
+
+?>
+
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>update</title>
+</head>
+
+<body>
+
+    <form method="POST" action="updateTeam.php?id=<?php echo $row['id'];?>">
+
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" value="<?php echo $row['nome'];?>" required>
+
+        <label for="cidade">Cidade:</label>
+        <input type="text" name="cidade" value="<?php echo $row['cidade'];?>" required>
+
+        <input type="submit" value="Atualizar">
+
+    </form>
+
+    <a href="readTeam.php">Ver times.</a>
+
+</body>
+
+</html>
