fcf9b4e6a30ce19d948936e47e8133de0f3d36bc
fismo <heitor_emmerich@estudante.sesisenai.org.br>
2025-08-22 08:36:31 -0300

    add - redirecionadores padrão

---
+++ private/Match/index.php
@@ -0,0 +1,2 @@
+<a href="createMatch.php">Criar Partida</a>
+<a href="ReadMatch.php">ver Partidas</a>
---
+++ private/Players/index.php
@@ -0,0 +1,2 @@
+<a href="createPlayer.php">Criar jogador</a>
+<a href="ReadPlayer.php">ver jogador</a>
