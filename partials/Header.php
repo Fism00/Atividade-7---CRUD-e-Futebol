909d8436f0345dcdc67708a8ba3b09a19517e961
fismo <heitor_emmerich@estudante.sesisenai.org.br>
2025-08-22 08:03:28 -0300

    add - Arquives

---
+++ partials/Header.php
@@ -0,0 +1,14 @@
+<?php
+
+include 'db.php';
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud e Futebol</title>
+    <link rel="stylesheet" href="../style/style.php">
+</head>
+<body>
+
